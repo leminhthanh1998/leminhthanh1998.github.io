096b36fa66b47b384516b8d0b0f09fd784d71f13
Le Minh Thanh <leminhthanh1998@outlook.com>
2018-07-02 15:37:27 +0700

    Upload LMT File Shredder 1.1

--- lmt-file-shredder/assets/js/main.js
+++ lmt-file-shredder/assets/js/main.js
@@ -1,25 +1,32 @@
 /*
-	Read Only by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Introspect by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
 (function($) {
 
 	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 1024px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)'
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
 	});
 
 	$(function() {
 
-		var $body = $('body'),
-			$header = $('#header'),
-			$nav = $('#nav'), $nav_a = $nav.find('a'),
-			$wrapper = $('#wrapper');
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
 
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
@@ -32,80 +39,32 @@
 				);
 			});
 
-		// Header.
-			var ids = [];
-
-			// Set up nav items.
-				$nav_a
-					.scrolly({ offset: 44 })
-					.on('click', function(event) {
-
-						var $this = $(this),
-							href = $this.attr('href');
-
-						// Not an internal link? Bail.
-							if (href.charAt(0) != '#')
-								return;
-
-						// Prevent default behavior.
-							event.preventDefault();
-
-						// Remove active class from all links and mark them as locked (so scrollzer leaves them alone).
-							$nav_a
-								.removeClass('active')
-								.addClass('scrollzer-locked');
-
-						// Set active class on this link.
-							$this.addClass('active');
-
-					})
-					.each(function() {
-
-						var $this = $(this),
-							href = $this.attr('href'),
-							id;
-
-						// Not an internal link? Bail.
-							if (href.charAt(0) != '#')
-								return;
-
-						// Add to scrollzer ID list.
-							id = href.substring(1);
-							$this.attr('id', id + '-link');
-							ids.push(id);
-
-					});
-
-			// Initialize scrollzer.
-				$.scrollzer(ids, { pad: 300, lastHack: true });
-
 		// Off-Canvas Navigation.
 
-			// Title Bar.
-				$(
-					'<div id="titleBar">' +
-						'<a href="#header" class="toggle"></a>' +
-						'<span class="title">' + $('#logo').html() + '</span>' +
-					'</div>'
-				)
+			// Navigation Panel Toggle.
+				$('<a href="#navPanel" class="navPanelToggle"></a>')
 					.appendTo($body);
 
-			// Header.
-				$('#header')
+			// Navigation Panel.
+				$(
+					'<div id="navPanel">' +
+						$('#nav').html() +
+						'<a href="#navPanel" class="close"></a>' +
+					'</div>'
+				)
+					.appendTo($body)
 					.panel({
 						delay: 500,
 						hideOnClick: true,
 						hideOnSwipe: true,
 						resetScroll: true,
 						resetForms: true,
-						side: 'right',
-						target: $body,
-						visibleClass: 'header-visible'
+						side: 'left'
 					});
 
-			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+			// Fix: Remove transitions on WP<10 (poor/buggy performance).
 				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#titleBar, #header, #wrapper')
+					$('#navPanel')
 						.css('transition', 'none');
 
 	});
